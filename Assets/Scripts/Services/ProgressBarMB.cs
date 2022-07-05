ee176f806905eb43601ef7217f2953c9250d4516
VasilyMa <vasilymigulin@gmail.com>
2022-07-05 11:51:47 +0300

    прогрессирующий бар

---
+++ Assets/Scripts/Services/ProgressBarMB.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace Client
+{
+    public class ProgressBarMB : MonoBehaviour
+    {
+        private EcsWorld _world;
+        private GameState _state;
+        [SerializeField] private Slider _slider;
+        [SerializeField] private Gradient _gradient;
+        [SerializeField] private Image _fill;
+        [SerializeField] private float _currentAmount;
+        [SerializeField] private float _maxAmount;
+        [SerializeField] private GameObject _progress;
+        //private EcsPool<DeadTag> _deadPool;
+        //private EcsPool<EnemyTag> _enemyPool;
+        public void Init(EcsWorld world, GameState state)
+        {
+            _world = world;
+            //_deadPool = world.GetPool<DeadTag>();
+            //_enemyPool = world.GetPool<EnemyTag>();
+            _state = state;
+        }
+        public void SetMaxAmount(float value)
+        {
+            _maxAmount = value;
+            _currentAmount = value;
+            _slider.maxValue = _maxAmount;
+            _slider.value = _currentAmount;
+
+        }
+        public void UpdateProgressBar()
+        {
+            var filter = _world.Filter<EnemyTag>().Inc<DeadTag>().End();
+            _currentAmount = _state.WaveStorage.GetAllEnemies() - filter.GetEntitiesCount();
+            _slider.value = _currentAmount;
+        }
+    }
+}
