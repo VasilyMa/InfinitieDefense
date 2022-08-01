ada12ce3f63028ba0ae459f81890d6f63014a2ae
VasilyMa <vasilymigulin@gmail.com>
2022-08-01 16:42:13 +0300

    таймер на победу

--- Assets/Scripts/Systems/CheckWinSystem.cs
+++ Assets/Scripts/Systems/CheckWinSystem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 namespace Client {
     sealed class CheckWinSystem : IEcsRunSystem {
         readonly EcsFilterInject<Inc<EnemyTag, UnitTag, DeadTag>> _filterEnemies = default;
+        readonly EcsFilterInject<Inc<CountdownWaveComponent>> _countdown = default;
         readonly EcsWorldInject _world;
         readonly EcsSharedInject<GameState> _state;
         readonly EcsPoolInject<CountdownWaveComponent> _countdownPool = default;
@@ -14,6 +15,10 @@ namespace Client {
                 ref var interfaceComponent = ref _interfacePool.Value.Get(_state.Value.EntityInterface);
                 if (_filterEnemies.Value.GetEntitiesCount() == _state.Value.WaveStorage.GetAllEnemies() && _state.Value.isWave)
                 {
+                    foreach (var countdown in _countdown.Value)
+                    {
+                        _countdown.Pools.Inc1.Del(countdown);
+                    }
                     _state.Value.isWave = false;
                     _countdownPool.Value.Add(_world.Value.NewEntity());
                     interfaceComponent.countdownWave.GetComponent<CountdownWaveMB>().SetTimer(30);
